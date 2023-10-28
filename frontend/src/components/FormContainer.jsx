15dbafc967b9e0522d81807e0410ef546c36195e
shakilshagu <muhammadshakil1968@gmail.com>
2023-10-28 19:05:44 +0530

    new commit

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -1,11 +1,13 @@
+
 import { Container, Row, Col } from 'react-bootstrap';
 
-const FormContainer = (children) => {
+// eslint-disable-next-line react/prop-types
+const FormContainer = ({children}) => {
   return (
     <Container>
       <Row className='justify-content-md-center mt-5'>
         <Col xs={12} md={6} className='card p-5'>
-          {children}
+        {children}
         </Col>
       </Row>
     </Container>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,37 @@
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
-function Header() {
+const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand>MERN VITE</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>MERN Auth</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <>
                 <NavDropdown id="username">
-                  <NavDropdown.Item>Profile</NavDropdown.Item>
-
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
                   <NavDropdown.Item>Logout</NavDropdown.Item>
                 </NavDropdown>
               </>
               <>
-                <Nav.Link>
-                  <FaSignInAlt /> Sign In
-                </Nav.Link>
-
-                <Nav.Link>
-                  <FaSignOutAlt /> Sign Up
-                </Nav.Link>
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <FaSignInAlt /> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/register">
+                  <Nav.Link>
+                    <FaSignOutAlt /> Sign Up
+                  </Nav.Link>
+                </LinkContainer>
               </>
             </Nav>
           </Navbar.Collapse>
@@ -32,6 +39,6 @@ function Header() {
       </Navbar>
     </header>
   );
-}
+};
 
 export default Header;
